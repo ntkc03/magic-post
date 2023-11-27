fb3141b68dfe05105dd5d041800b24cb8cd0f67d
kimchiiiiii <21020287@vnu.edu.vn>
2023-11-27 11:56:01 +0700

    edit homepage

--- client/src/components/homepage/HomePage.tsx
+++ client/src/components/homepage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux/es/exports";
 import { ToastContainer, toast } from "react-toastify";
+import { Disclosure } from "@headlessui/react";
 import "react-toastify/dist/ReactToastify.css";
 
 
@@ -18,8 +19,20 @@ export default function HomePage() {
       : toast.success(msg, { position: toast.POSITION.TOP_RIGHT });
 
   return (
-    <div>
-        <div className="flex justify-center h-screen bg-background"></div>
-    </div>
+    <Disclosure as="div" className="">
+    {({ open }) => (
+      <>
+      <div className="flex flex-col items-center justify-center h-screen bg-background text-center text-textColor">
+        <h1 className="text-8xl my-4">Magic derives from <br /> sustainable service</h1>
+        <p className="text-xl">MagicPost is a magical provider of domestic and <br />international parcel courier services in Vietnam.<br /><br /></p>
+        <div className="mb-14">
+          <Disclosure.Button className="rounded-md p-2 sm:bg-buttonbg bg-buttonBlue hover:underline sm:text-white text-textColor focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <a href = "">Search</a>
+          </Disclosure.Button>
+        </div>
+      </div>
+      </>
+    )}
+    </Disclosure>
   );
 }
