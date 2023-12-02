0afdddb1c1526fe941e0baadfbc363996fb19937
kimchiiiiii <21020287@vnu.edu.vn>
2023-12-02 21:48:11 +0700

    edit homepage, add footer

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import CommonHeader from '../../components/header/CommonHeader'
 import HomePage from '../../components/homepage/HomePage'
+import UserSideFooter from '../../components/footer/UserSideFooter'
 
 function Home() {
   return (
     <div>
         <CommonHeader/>
         <HomePage/>
+        <UserSideFooter/>
     </div>
   )
 }
