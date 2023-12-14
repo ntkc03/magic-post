faf8c3b6695557d5be000ebd99dc11e4b3ed6472
ntkc03 <nguyenthikieuchinh2801@gmail.com>
2023-12-14 17:56:45 +0700

    fix

--- client/src/routes/order/OrderRouter.tsx
+++ client/src/routes/order/OrderRouter.tsx
@@ -1,15 +1,14 @@
 import { Route, Routes, useParams } from "react-router-dom";
 import CreateOrderPage from "../../pages/order/CreateOrderPage";
-import OrderDetailsPage from "../../pages/order/orderDetailsPage";
 import OrderPrintFormPage from "../../pages/order/OrderPrintPage";
 import OrderListPage from "../../pages/order/orderListPage";
 import SearchPage from "../../pages/order/searchPage";
 
-function OrderDetailsPageWrapper() {
-  const { code } = useParams();
-  const codeValue = code || '';
-  return <OrderDetailsPage code={codeValue} />;
-}
+// function OrderDetailsPageWrapper() {
+//   const { code } = useParams();
+//   const codeValue = code || '';
+//   return <OrderDetailsPage code={codeValue} />;
+// }
 
 function OrderPrintFormPageWrapper() {
   const { code } = useParams();
@@ -22,7 +21,7 @@ const OrderRouter = () => {
     <div>
       <Routes>
         <Route path="/new" element={<CreateOrderPage />} />
-        <Route path="/details/:code" element={<OrderDetailsPageWrapper/>} />
+        {/* <Route path="/details/:code" element={<OrderDetailsPageWrapper/>} /> */}
         <Route path="/print/:code" element={<OrderPrintFormPageWrapper/>} />
         <Route path="/list" element={<OrderListPage/>} />
         <Route path="/search" element={<SearchPage/>} />
